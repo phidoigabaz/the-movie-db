481c362d23f4dd2ca66856e7900074681e815cd4
Tạ Thành Đạt <dattt@gemvietnam.com>
2019-05-27 09:35:55 +0700

    Initial Commit

---
+++ TheNewMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheNewMovie
+//
+//  Created by Tạ Thành Đạt on 5/27/19.
+//  Copyright © 2019 dattt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
